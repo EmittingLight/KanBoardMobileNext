6ab364c4b72fba498f30088aa9e037b6ec4f930d
John Doe <johndoe@example.com>
2025-05-26 12:18:41 +0300

    Решил проблему с статус баром на втором при добавлении новой задачи и добавил стрелку назад

--- app/src/main/java/com/yaga/kanboardmobile/AddTicketActivity.java
+++ app/src/main/java/com/yaga/kanboardmobile/AddTicketActivity.java
@@ -1,19 +1,32 @@
 package com.yaga.kanboardmobile;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddTicketActivity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
 
+public class AddTicketActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 💥 Жестко возвращаем статус-бар
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
         setContentView(R.layout.activity_add_ticket);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar_add);
+        toolbar.setNavigationOnClickListener(v -> finish());
 
         EditText editTextTitle = findViewById(R.id.editTextTitle);
         EditText editTextDescription = findViewById(R.id.editTextDescription);
